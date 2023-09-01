bf68943cb1bc0623f9f72336915ee10c506e9b70
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2023-09-01 12:10:19 +0330

    second version

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,67 +1,66 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null)
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  //fetch data using fetch
+  // fetch data using fetch
   // useEffect(() => {
-  //   const abortCont = new AbortController()
-  //   setTimeout(() => {
-  //     fetch(url, { signal: abortCont.signal })
-  //       .then((res) => {
-  //         if (!res.ok) {
-  //           throw Error("could not fetch data for that result")
-  //         }
-  //         return res.json()
-  //       })
-  //       .then((data) => {
-  //         setData(data)
-  //         setIsPending(false)
-  //         setError(null)
-  //       })
-  //       .catch((err) => {
-  //         if (err.name === "AbortError") {
-  //           console.log("fetch aborted")
-  //         } else {
-  //           setError(err.message)
-  //           setIsPending(false)
-  //         }
-  //       })
-  //   }, 1000)
-  //   return () => abortCont.abort()
-  // }, [url])
-  // return { data, isPending, error }
+  //   const abortController = new AbortController();
+  //   setTimeout(async () => {
+  //     try {
+  //       const response = await fetch(url, { signal: abortController.signal });
+  //       if (response.status >= 200 && response.status < 300) {
+  //         const data = await response.json();
+  //         setData(data);
+  //         setIsPending(false);
+  //         setError(null);
+  //       } else {
+  //         throw new Error(`${response.status}: ${response.statusText}`);
+  //       }
+  //     } catch (error) {
+  //       if (error.name === "AbortError") {
+  //         console.log("fetch aborted");
+  //       } else {
+  //         setError(error.message);
+  //         setIsPending(false);
+  //       }
+  //     } finally {
+  //       return () => abortController.abort();
+  //     }
+  //   }, 1000);
+  // }, [url]);
 
   // fetch data using axios
   useEffect(() => {
-    const cancelToken = axios.CancelToken
-    const source = cancelToken.source()
-    setTimeout(() => {
-      const fetchData = async () => {
-        setError(null)
-        setIsPending(true)
-        try {
-          const res = await axios.get(url, { cancelToken: source.token })
-          setData(res.data)
-          setIsPending(false)
-          setError(null)
-        } catch (error) {
-          if (axios.isCancel(error)) {
-            return "axios request cancelled"
-          } else {
-            setError(error)
-            setIsPending(false)
-          }
+    const cancelToken = axios.CancelToken;
+    const source = cancelToken.source();
+    setTimeout(async () => {
+      try {
+        const response = await axios.get(url, { cancelToken: source.token });
+        if (response.status >= 200 && response.status < 300) {
+          setData(response.data);
+          setIsPending(false);
+          setError(null);
+        } else {
+          throw new Error(`${response.status}: ${response.statusText}`);
         }
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log('axios request cancelled');
+        } else {
+          setError(error.message);
+          setIsPending(false);
+        }
+      } finally {
+        return () => source.cancel('axios request cancelled');
       }
-      fetchData()
-    }, 1000)
-    return () => source.cancel("axios request cancelled")
-  }, [url])
-  return { data, isPending, error }
-}
+    }, 1000);
+  }, [url]);
 
-export default useFetch
+  return { data, isPending, error };
+};
+
+export default useFetch;
